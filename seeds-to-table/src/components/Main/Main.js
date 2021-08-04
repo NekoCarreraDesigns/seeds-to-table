d0e51750c97dbbd00f8e882470cf32e2e581b6b7
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-04 16:26:04 -0600

    added image to main

---
+++ seeds-to-table/src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Main.css";
+
+function Main() {
+  return (
+    <div>
+      <img
+        className='image-of-food'
+        alt='table-of-food'
+        src='https://images.unsplash.com/photo-1481931098730-318b6f776db0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'></img>
+      <p></p>
+    </div>
+  );
+}
+export default Main;
