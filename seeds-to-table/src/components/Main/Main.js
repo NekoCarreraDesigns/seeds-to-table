d7f42099e63e23f7f57fcbec98af58e990eb0df0
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-04 16:44:16 -0600

    added image and paragraph to site

--- seeds-to-table/src/components/Main/Main.js
+++ seeds-to-table/src/components/Main/Main.js
@@ -8,7 +8,11 @@ function Main() {
         className='image-of-food'
         alt='table-of-food'
         src='https://images.unsplash.com/photo-1481931098730-318b6f776db0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'></img>
-      <p></p>
+      <aside className='about-joe-seeds'>
+        Joe Seeds is a classically trained chef, climber, snowboarder, and ready
+        to cook your next dinner party. Born in Philadelphia he learned how to
+        cook from watching Martin Yan, The Frugal gourmet, and Gordon Ramsay
+      </aside>
     </div>
   );
 }
