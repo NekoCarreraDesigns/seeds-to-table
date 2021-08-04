9c347674fcb57cafe3fedf7f2c07dea1358472c9
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-04 15:26:20 -0600

    removed app.css

--- seeds-to-table/src/components/Header/Header.js
+++ seeds-to-table/src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Header.css";
 
-function Header () {
-    return (
-        <div>
-          <h1 className="page-title">Seeds to Table</h1>
-        </div>
-    )
+function Header() {
+  return (
+    <div>
+      <h1 className='page-title'>Seeds to Table</h1>
+    </div>
+  );
 }
- 
-export default Header
+
+export default Header;
