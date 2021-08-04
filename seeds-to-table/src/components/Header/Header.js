1574dfe968563d3f1db77ad4231ad0e163efb7ed
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-04 13:57:10 -0600

    added fontawesome script tag

--- seeds-to-table/src/components/Header/Header.js
+++ seeds-to-table/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import "./Header.css";
 function Header () {
     return (
         <div>
-            <h1 className="page-title">Seeds to Table</h1>
+           <h1 className="page-title">Seeds to Table</h1>
         </div>
     )
 }
