8c8b16823f3f0414e1129990f38043318a43cdc6
NekoCarreraDesigns <denvermaas@gmail.com>
2021-08-04 15:42:55 -0600

    added footer and styled footer

---
+++ seeds-to-table/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className='text-center container-fluid'>
+      <div className='card-footer'>
+        <small className='text-red mx-auto'>
+          &copy; 2021 • Made by Neko Carrera Designs All Rights Reserved
+        </small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
